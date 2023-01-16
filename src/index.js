efc6e839a65e890a1b38df3312e71067a8901fea
yyaskriloff <69407772+yyaskriloff@users.noreply.github.com>
2023-01-16 15:47:32 +0200

    implenent Links and feed resolver

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import { ApolloServer } from 'apollo-server'
+
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+}]
+
+const typeDefs = `
+  type Query {
+    info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
+  }
+`
+const resolvers = {
+    Query: {
+        info: () => `This is the API of a Hackernews Clone`,
+
+        feed: () => links,
+    },
+    Link: {
+        id: (parent) => parent.id,
+        description: (parent) => parent.description,
+        url: (parent) => parent.url,
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+})
+
+server
+    .listen()
+    .then(({ url }) =>
+        console.log(`Server is running on ${url}`)
+    );
